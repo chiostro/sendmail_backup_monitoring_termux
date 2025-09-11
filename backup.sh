a9779f3adf63614f144e94b3673de727953856cf
chiostro <notoand@gmail.com>
2025-09-11 18:07:41 +0200

    Update backup.sh

--- backup.sh
+++ backup.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+# Author Andrea Noto
+# Lì mo  11 Sep 2026
 test -z$1 && echo "As parameter $0 folder path. Default is  Download"
 SOURCE_DIR=${1:-/storage/emulated/0/Download}
 WKPATH=`pwd`
