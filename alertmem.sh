fa664646aa19f3d82f955f6daff49565cc77b67a
chiostro <notoand@gmail.com>
2025-09-11 18:20:56 +0200

    Update alertmem.sh

--- alertmem.sh
+++ alertmem.sh
@@ -1,1 +1,12 @@
-echo "Deve venire"
+#!/bin/bash
+#Author Andrea Noto
+test -f ~/.msmtprc && receiver=` awk '$1=="from" { print $2}' ~/.msmtprc `
+THRESHOLD=80
+USED_MEM=$(free | grep Mem | awk '{print $3/$2 * 100}')
+USED_ME=`echo $USED_MEM|awk -F\. '{ print $1  }'`
+if [ $USED_ME -gt $THRESHOLD ]; then
+  echo "Attenzione: uso memoria greater then $THRESHOLD%" 
+  test -z $receiver ||  echo  "Subject: Mobile Memory Treashold $USED_MEM\n\nHi, \n clean memory of your mobile ." | msmtp   $receiver
+
+fi
+
