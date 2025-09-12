6c51de0b7ec3b57f1dbc4da49ab8a7ecf36b9d76
Cybersec <12345678+chiostro@users.noreplaygithub.com>
2025-09-12 08:20:20 +0200

    Aggiornato  alertmem.sh con le modifiche necessarie

--- alertmem.sh
+++ alertmem.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
-#Author Andrea Noto
 test -f ~/.msmtprc && receiver=` awk '$1=="from" { print $2}' ~/.msmtprc `
 THRESHOLD=80
+MOBILE=`getprop ro.product.brand`
 USED_MEM=$(free | grep Mem | awk '{print $3/$2 * 100}')
 USED_ME=`echo $USED_MEM|awk -F\. '{ print $1  }'`
 if [ $USED_ME -gt $THRESHOLD ]; then
-  echo "Attenzione: uso memoria greater then $THRESHOLD%" 
-  test -z $receiver ||  echo  "Subject: Mobile Memory Treashold $USED_MEM\n\nHi, \n clean memory of your mobile ." | msmtp   $receiver
+  echo "Attenzione: uso memoria greater then $THRESHOLD% $MOBILE" 
+  test -z $receiver ||  echo  "Subject: Mobile Memory Treashold $USED_MEM\n\nHi, \n clean memory of your $MOBILE mobile ." | msmtp   $receiver
 
 fi
 
