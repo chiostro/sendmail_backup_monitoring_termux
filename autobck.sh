1890ef1a6a2e0022acbc3ff61d070f8263c5e729
chiostro <notoand@gmail.com>
2025-09-15 14:16:26 +0200

    Update autobck.sh

--- autobck.sh
+++ autobck.sh
@@ -3,7 +3,7 @@
 receiver=` awk '$1=="from" { print $2}' ~/.msmtprc `
 ./backup.sh
 Pass=`ls -rt ../password-manager-bash/password_store.*.gpg 2>/dev/null|tail -1`
-echo "Type passphrase for $Pass and automatically password inside will be used to crypt backup"
+echo "Type passphrase for $Pass and automatically the stored hidden password  will be used to crypt backup"
 if [ -f "$Pass" ];then
    pass=`gpg --quiet --pinentry-mode loopback --decrypt $Pass `
 else
@@ -15,8 +15,8 @@ tar -tzf $bckupname >> list.txt
 sh ../obfuscate-file-gpg/obfuscate.sh $bckupname  $pass
 cat list.txt | mutt -s "$bckname.gpg password is crypted in $Pass" -a list.txt -- $receiver
 RCLONED=`rclone listremotes|tail -1`
-#echo "Upload $bckupname.gpg into $RCLONED"
-#test -z $RCLONED || rclone copy  $bckupname.gpg $RCLONED
+echo "rclone copy  $bckupname.gpg $RCLONED"
+test -z $RCLONED || rclone copy  $bckupname.gpg $RCLONED
 echo "Move $bckupname.gpg  under /storage/emulated/0"
 mv $bckupname.gpg  /storage/emulated/0
 sleep 5
